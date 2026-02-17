20b6806610c916bb279bbd718e6248c965c3fd86
Sch <junhg0211@gmail.com>
2026-02-18 01:27:24 +0900

    Update Svelte import paths and fix syntax issues

--- src/routes/dictionary/+page.svelte
+++ src/routes/dictionary/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import { page } from '$app/stores';
+  import { onMount } from "svelte";
+  import { page } from "$app/stores";
 
   const KEY = "AIzaSyD-HRSmm2OBOf0ZpMh5kAfPFggwXLVPC6E";
   const SPREADSHEET_ID = "1vTe1JGdWz95CRl4-Kg8b5KVHZ25tLsqx1aSAOCxpT1A";
 
   let values;
-  let searchTerm = '';
+  let searchTerm = "";
 
   function searchWord(term: string) {
     if (!values) return [];
@@ -28,31 +28,38 @@
 
   function wordSrcFor(word: string) {
     if ($page.url.host === "localhost:5173") {
-      return `/laul/${word}.jpg`
+      return `/laul/${word}.jpg`;
     } else {
-      return `/laul/laul/${word}.jpg`
+      return `/laul/laul/${word}.jpg`;
     }
   }
 
   onMount(() => {
-    fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/단어?key=${KEY}`)
-      .then(response => response.json())
-      .then(data => {
+    fetch(
+      `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/단어?key=${KEY}`,
+    )
+      .then((response) => response.json())
+      .then((data) => {
         values = data.values.splice(1);
       })
-      .catch(error => {
-        console.error('Error fetching data:', error);
+      .catch((error) => {
+        console.error("Error fetching data:", error);
       });
   });
 </script>
 
-<input type="text" placeholder="검색어를 입력하세요" bind:value={searchTerm} class="query" />
+<input
+  type="text"
+  placeholder="검색어를 입력하세요"
+  bind:value={searchTerm}
+  class="query"
+/>
 {#if values}
   <div class="results">
     {#each searchWord(searchTerm) as word}
       <div class="word">
-        <div class="title"><img src={wordSrcFor(word[1])}> {word[1]}</div>
-        <div class="meaning">{word[3]}</div>
+        <div class="title"><img src={wordSrcFor(word[1])} /> {word[1]}</div>
+        <div class="meaning">{word[2]}</div>
       </div>
     {/each}
   </div>
--- src/routes/wordmap/+page.svelte
+++ src/routes/wordmap/+page.svelte
@@ -1,59 +1,70 @@
 <script lang="ts">
-  import cytoscape from 'cytoscape';
-  import { onMount } from 'svelte';
-  import { page } from '$app/stores';
-  import fcose from 'cytoscape-fcose';
+  import cytoscape from "cytoscape";
+  import { onMount } from "svelte";
+  import { page } from "$app/stores";
+  import fcose from "cytoscape-fcose";
 
   cytoscape.use(fcose);
 
   onMount(() => {
     const cy = cytoscape({
-      container: document.getElementById('cy'),
+      container: document.getElementById("cy"),
       style: [
         {
-          selector: 'node',
+          selector: "node",
           style: {
-            'background-color': '#666',
-            label: 'data(label)',
-            'text-valign': 'bottom',
-            'color': 'grey',
-            'background-image': 'data(img)',
-            'background-fit': 'cover',
-            'shape': 'rectangle',
-          }
+            "background-color": "white",
+            label: "data(label)",
+            "text-valign": "bottom",
+            color: "grey",
+            "background-image": "data(img)",
+            "background-fit": "contain",
+            shape: "rectangle",
+          },
         },
         {
-          selector: 'edge',
+          selector: "edge",
           style: {
             width: 3,
-            'line-color': '#ccc',
-            'target-arrow-color': '#ccc',
-            'target-arrow-shape': 'triangle',
-            'curve-style': 'bezier'
-          }
-        }
+            "line-color": "#ccc",
+            "target-arrow-color": "#ccc",
+            "target-arrow-shape": "triangle",
+            "curve-style": "bezier",
+          },
+        },
       ],
       layout: {
-        name: 'fcose',
-        quality: 'proof',
+        name: "fcose",
+        quality: "proof",
         randomize: true,
-        animate: true
-      }
+        animate: true,
+      },
     });
 
     const KEY = "AIzaSyD-HRSmm2OBOf0ZpMh5kAfPFggwXLVPC6E";
     const SPREADSHEET_ID = "1vTe1JGdWz95CRl4-Kg8b5KVHZ25tLsqx1aSAOCxpT1A";
-    fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/단어?key=${KEY}`)
-      .then(response => response.json())
-      .then(data => {
+    fetch(
+      `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/단어?key=${KEY}`,
+    )
+      .then((response) => response.json())
+      .then((data) => {
         const values = data.values.splice(1);
 
         const nodes = [];
         const edges = [];
         for (const word of values) {
-          nodes.push({ data: { id: word[0], label: word[1], img: $page.url.host === "localhost:5173" ? `/laul/${word[1]}.jpg` : `/laul/laul/${word[1]}.jpg` } });
-          if (word[4] !== "-") {
-            const relatedWords = word[4].split(',').map(w => w.trim());
+          nodes.push({
+            data: {
+              id: word[0],
+              label: word[1],
+              img:
+                $page.url.host === "localhost:5173"
+                  ? `/laul/${word[1]}.jpg`
+                  : `/laul/laul/${word[1]}.jpg`,
+            },
+          });
+          if (word[3] !== "-") {
+            const relatedWords = word[3].split(",").map((w) => w.trim());
             for (const relatedWord of relatedWords) {
               edges.push({ data: { source: relatedWord, target: word[0] } });
             }
@@ -62,10 +73,10 @@
 
         cy.add(nodes);
         cy.add(edges);
-        cy.layout({ name: 'cose' }).run();
+        cy.layout({ name: "cose" }).run();
       })
-      .catch(error => {
-        console.error('Error fetching data:', error);
+      .catch((error) => {
+        console.error("Error fetching data:", error);
       });
   });
 </script>
